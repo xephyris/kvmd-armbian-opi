14bbf7e48bef27fe0b565ac69908e9528dedd44a
srepac <68381903+srepac@users.noreply.github.com>
2023-03-23 10:58:34 -0700

    Update fix-nfs-msd.sh

--- fix-nfs-msd.sh
+++ fix-nfs-msd.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+# Written by srepac (c) 2023
+#
+# This is meant for armbian PiKVM use -- required to make NFS MSD work properly
+#
+# In summary, this script will perform the following steps:
+: "
+1.  take the contents of /usr/lib/python3.10/site-packages/aiofiles from arch pikvm and extract it to armbian
+2.  rename aiofiles within /usr/lib/python3/dist-packages to aiofiles.YYYYMMDD.hhmm
+3.  create symlink to /usr/lib/python3.10/site-packages/aiofiles into /usr/lib/python3/dist-packages
+4.  restart kvmd
+"
+
 # first, check to make sure that NFS_Primary is mounted in /var/lib/kvmd/msd
 if [ $(mount | grep -c NFS_Primary) -le 0 ]; then
   echo "Missing /var/lib/kvmd/msd/NFS_Primary mount"
-  echo "Please follow https://docs.pikvm.org/msd/#nfs-storage" 
+  echo "Please follow https://docs.pikvm.org/msd/#nfs-storage"
   exit 1
 else
   echo "/var/lib/kvmd/msd/NFS_Primary is mounted."
@@ -19,8 +31,8 @@ wget -O /tmp/$NAME $AIOFILES > /dev/null 2> /dev/null
 echo "done"
 
 LOCATION="/usr/lib/python3.10/site-packages"
-echo "-> Extracting /tmp/$NAME into $LOCATION" 
-tar xvf /tmp/$NAME -C $LOCATION 
+echo "-> Extracting /tmp/$NAME into $LOCATION"
+tar xvf /tmp/$NAME -C $LOCATION
 
 echo "-> Renaming original aiofiles and creating symlink to correct aiofiles"
 cd /usr/lib/python3/dist-packages
@@ -31,4 +43,4 @@ ls -ld aiofiles*
 echo "-> Restarting kvmd"
 systemctl restart kvmd
 
-echo "-> Please check to make sure that NFS MSD is working"
+echo "-> Please check webui to make sure that NFS MSD is working properly."
