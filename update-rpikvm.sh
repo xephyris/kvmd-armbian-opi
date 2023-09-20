e0706fe9bebabe30c207a07f09d28b0543c3fd5c
srepac <68381903+srepac@users.noreply.github.com>
2023-09-20 09:32:14 -0700

    Update update-rpikvm.sh
    remove OCR support if SBC has <= 256MB RAM

--- update-rpikvm.sh
+++ update-rpikvm.sh
@@ -213,11 +213,12 @@ misc-fixes() {
 
 fix-python311() {
   printf "\n-> python3.11 kvmd path fix\n\n"
-  cd /usr/lib/python3/dist-packages/; ls -ld kvmd*
+  cd /usr/lib/python3/dist-packages/
+  ls -ld kvmd
+  ls -ld kvmd-[0-9]* | tail -2  # show last 2 kvmd-*info links
 
   if [ $( ls -ld kvmd | grep -c 3.10 ) -gt 0 ]; then
     ln -sf /usr/lib/python3.11/site-packages/kvmd .
-    ls -ld kvmd*
   else
     printf "\nkvmd is already symlinked to python3.11 version.  Nothing to do.\n"
   fi
@@ -229,13 +230,13 @@ fix-nfs-msd() {
 
   LOCATION="/usr/lib/python3.11/site-packages"
   echo "-> Extracting $NAME into $LOCATION"
-  tar xvf $NAME -C $LOCATION
+  tar xvf $NAME -C $LOCATION > /dev/null
 
   echo "-> Renaming original aiofiles and creating symlink to correct aiofiles"
   cd /usr/lib/python3/dist-packages
   mv aiofiles aiofiles.$(date +%Y%m%d.%H%M)
-  ln -s $LOCATION/aiofiles .
-  ls -ld aiofiles*
+  ln -sf $LOCATION/aiofiles .
+  ls -ltrd aiofiles* | tail -2
 }
 
 fix-nginx() {
@@ -337,7 +338,16 @@ misc-fixes
 fix-python311
 fix-nfs-msd
 fix-nginx
-ocr-fix
+RAM=$( pistat | grep '^#' | awk '{print $NF}' )
+if [ $( echo $RAM | grep -c 256MB ) -eq 0 ]; then
+  # RAM > 256MB so we can support OCR (and perform OCR-fix)
+  ocr-fix
+else
+  echo
+  echo "-> Too low RAM [ $RAM ] onboard to support OCR.  Removing tesseract packages"
+  apt remove -y tesseract-ocr tesseract-ocr-eng > /dev/null 2> /dev/null
+  echo
+fi
 
 ln -sf python3 /usr/bin/python
 
