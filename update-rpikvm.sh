0cd64a2a90d7ff50ec402c7627a36d605dc68a85
srepac <68381903+srepac@users.noreply.github.com>
2024-04-08 20:54:37 -0700

    Update update-rpikvm.sh

--- update-rpikvm.sh
+++ update-rpikvm.sh
@@ -432,7 +432,7 @@ if systemctl is-enabled -q dnsmasq; then
 fi
 
 ### fix kvmd-webterm 0.49 change that changed ttyd to kvmd-ttyd which broke webterm
-sed -i -e ‘s/kvmd-ttyd/ttyd/g’ /lib/systemd/system/kvmd-webterm.service
+sed -i -e 's/kvmd-ttyd/ttyd/g' /lib/systemd/system/kvmd-webterm.service
 systemctl restart kvmd-webterm
 
 ### if kvmd service is enabled, then restart service and show message ###
