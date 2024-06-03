10c21327124fdbe6699ad750465565572dbe27ac
srepac <68381903+srepac@users.noreply.github.com>
2024-06-02 22:49:51 -0700

    Update install-x86.sh

--- install-x86.sh
+++ install-x86.sh
@@ -317,7 +317,7 @@ install-kvmd-pkgs() {
 
   # uncompress janus package if /usr/bin/janus doesn't exist
   if [ ! -e /usr/bin/janus ]; then
-    i=$( ls ${KVMDCACHE}/*.tar.xz | egrep janus )
+    i=$( ls ${KVMDCACHE}/*.tar.xz | egrep janus | grep -v 1x )
     echo "-> Extracting package $i into /" | tee -a $INSTLOG
     tar xfJ $i
 
