379525dd4aff7c3c0ff5487d7f1fc14000033d95
srepac <68381903+srepac@users.noreply.github.com>
2024-09-26 09:59:53 -0700

    Update install-x86.sh
    replace all wget http:// to https://

--- install-x86.sh
+++ install-x86.sh
@@ -719,14 +719,14 @@ fix-nfs-msd() {
 }
 
 add-ch9329-support() {
-  wget --no-check-certificate -O install-ch9329.sh http://148.135.104.55/PiKVM/install-ch9329.sh 2> /dev/null
+  wget --no-check-certificate -O install-ch9329.sh https://148.135.104.55/PiKVM/install-ch9329.sh 2> /dev/null
   chmod +x install-ch9329.sh
   ./install-ch9329.sh
 }
 
 apply-x86-mods() {
   TARBALL="x86-mods.tar"
-  wget --no-check-certificate -O $TARBALL http://148.135.104.55/RPiKVM/$TARBALL 2> /dev/null
+  wget --no-check-certificate -O $TARBALL https://148.135.104.55/RPiKVM/$TARBALL 2> /dev/null
 
   if [ -e $TARBALL ]; then
     echo "-> Making backup of files that require modification" | tee -a $LOGFILE
@@ -762,7 +762,7 @@ fix-nginx() {
   cat $HTTPSCONF | tee -a $LOGFILE
 
   if [[ ! -e /usr/local/bin/pikvm-info || ! -e /tmp/pacmanquery ]]; then
-    wget --no-check-certificate -O /usr/local/bin/pikvm-info http://148.135.104.55/PiKVM/pikvm-info 2> /dev/null
+    wget --no-check-certificate -O /usr/local/bin/pikvm-info https://148.135.104.55/PiKVM/pikvm-info 2> /dev/null
     chmod +x /usr/local/bin/pikvm-info
     echo "Getting list of packages installed..." | tee -a $LOGFILE
     pikvm-info > /dev/null    ### this generates /tmp/pacmanquery with list of installed pkgs
@@ -843,7 +843,7 @@ x86-fix-3.256() {
   cp hw.py hw.py.$( date +%Y%m%d )
   #wget --no-check-certificate https://raw.githubusercontent.com/pikvm/kvmd/cec03c4468df87bcdc68f20c2cf51a7998c56ebd/kvmd/apps/kvmd/info/hw.py 2> /dev/null
   #mv hw.py.1 hw.py
-  wget --no-check-certificate -O hw.py http://148.135.104.55/PiKVM/TESTING/hw.py 2> /dev/null
+  wget --no-check-certificate -O hw.py https://148.135.104.55/PiKVM/TESTING/hw.py 2> /dev/null
 } # end x86-fix-3.256
 
 x86-fix-3.281() {
@@ -885,7 +885,7 @@ function fix-hk4401() {
 
   # Download kvmd-4.2 package from kvmnerds.com to /tmp and extract only the xh_hk4401.py script
   cd /tmp
-  wget --no-check-certificate -O kvmd-4.2-1-any.pkg.tar.xz http://148.135.104.55/REPO/NEW/kvmd-4.2-1-any.pkg.tar.xz 2> /dev/null
+  wget --no-check-certificate -O kvmd-4.2-1-any.pkg.tar.xz https://148.135.104.55/REPO/NEW/kvmd-4.2-1-any.pkg.tar.xz 2> /dev/null
   tar xvfJ kvmd-4.2-1-any.pkg.tar.xz --wildcards --no-anchored 'xh_hk4401.py'
 
   # Show diff of 4.2 version of xh_hk4401.py vs. current installed version
